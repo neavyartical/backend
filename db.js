e9678aacb32fb210800ef7053941d92a8ca5a824
neavyartical <neavyartical@gmail.com>
2026-04-11 18:16:01 +0100

    db.js

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected ✅");
-  } catch (error) {
-    console.error("MongoDB error ❌", error);
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB Error:", err);
     process.exit(1);
   }
 };
