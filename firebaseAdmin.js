b5429b9a135e83010e65567c23c43326d018f53d
neavyartical <neavyartical@gmail.com>
2026-04-14 19:34:51 +0100

    firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,1 +1,20 @@
 
+const admin = require("firebase-admin");
+
+let app;
+
+if (!admin.apps.length) {
+  try {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+    app = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
+    });
+
+    console.log("🔥 Firebase Admin initialized");
+  } catch (error) {
+    console.log("❌ Firebase init error:", error.message);
+  }
+}
+
+module.exports = admin;
