317a0372f7cdce7b08f6977b6351b42ec2808ce0
neavyartical <neavyartical@gmail.com>
2026-04-14 16:32:30 +0100

    firebaseadadmin.js

--- firebaseadadmin.js
+++ firebaseadadmin.js
@@ -1,1 +1,9 @@
 
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceaccountkey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+module.exports = admin;
