edf97e1931c53ec7cf6da3d0f06f676705b14d1c
neavyartical <neavyartical@gmail.com>
2026-04-16 20:31:33 +0100

    transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -1,1 +1,15 @@
 
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+  email: String,
+  type: String,
+  amount: Number,
+  description: String,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Transaction", transactionSchema);
