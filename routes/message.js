d119981d99f69161a179c79fb5a9039e4a70e511
neavyartical <neavyartical@gmail.com>
2026-04-19 15:56:07 +0100

    message.js

--- routes/message.js
+++ routes/message.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  sendMessage,
+  getMessages
+} = require("../controllers/messageController");
+
+router.post("/send", sendMessage);
+router.get("/:userId/:targetId", getMessages);
+
+module.exports = router;
 
