39dbb7f4a7566ba9099dafc0c409f2aef89776bf
neavyartical <neavyartical@gmail.com>
2026-04-19 06:46:26 +0100

    Call.js

--- models/Call.js
+++ models/Call.js
@@ -1,1 +1,39 @@
+import mongoose from "mongoose";
 
+const callSchema = new mongoose.Schema(
+  {
+    callerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    callType: {
+      type: String,
+      enum: ["audio", "video"],
+      default: "audio"
+    },
+
+    status: {
+      type: String,
+      enum: ["missed", "answered", "rejected"],
+      default: "missed"
+    },
+
+    duration: {
+      type: Number,
+      default: 0
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.models.Call || mongoose.model("Call", callSchema);
