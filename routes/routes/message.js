312eb67ae6cddfe6dfeb1ba2e37706f8c32064f1
neavyartical <neavyartical@gmail.com>
2026-04-19 06:59:43 +0100

    message.js

--- routes/routes/message.js
+++ routes/routes/message.js
@@ -1,1 +1,19 @@
+import express from "express";
+import {
+  sendMessage,
+  getMessages,
+  markSeen,
+  deleteMessage
+} from "../controllers/messageController.js";
 
+const router = express.Router();
+
+/* =========================
+   MESSAGE ROUTES
+========================= */
+router.post("/send", sendMessage);
+router.get("/:userId", getMessages);
+router.post("/seen/:userId", markSeen);
+router.delete("/:id", deleteMessage);
+
+export default router;
