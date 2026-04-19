a969f0093acce6d843128924bfd14bb32e0042fa
neavyartical <neavyartical@gmail.com>
2026-04-19 06:42:10 +0100

    User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,81 @@
-
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: String,
-  credits: { type: Number, default: 10 },
-  isOwner: { type: Boolean, default: false },
-  subscription: { type: String, default: "free" }
-});
+const userSchema = new mongoose.Schema(
+  {
+    uid: {
+      type: String,
+      unique: true,
+      sparse: true
+    },
 
-export default mongoose.model("User", userSchema);
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    username: {
+      type: String,
+      default: ""
+    },
+
+    avatar: {
+      type: String,
+      default: ""
+    },
+
+    bio: {
+      type: String,
+      default: ""
+    },
+
+    credits: {
+      type: Number,
+      default: 10
+    },
+
+    isOwner: {
+      type: Boolean,
+      default: false
+    },
+
+    subscription: {
+      type: String,
+      default: "free"
+    },
+
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+
+    likesReceived: {
+      type: Number,
+      default: 0
+    },
+
+    online: {
+      type: Boolean,
+      default: false
+    },
+
+    lastSeen: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
