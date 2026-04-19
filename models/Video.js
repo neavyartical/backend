5d5d2f4df598fdaab92bef8417b8ae205e22d23f
neavyartical <neavyartical@gmail.com>
2026-04-19 06:43:23 +0100

    Video.js

--- models/Video.js
+++ models/Video.js
@@ -1,1 +1,63 @@
+import mongoose from "mongoose";
 
+const videoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    caption: {
+      type: String,
+      default: ""
+    },
+
+    videoUrl: {
+      type: String,
+      required: true
+    },
+
+    thumbnail: {
+      type: String,
+      default: ""
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+
+    commentsCount: {
+      type: Number,
+      default: 0
+    },
+
+    shares: {
+      type: Number,
+      default: 0
+    },
+
+    views: {
+      type: Number,
+      default: 0
+    },
+
+    duration: {
+      type: Number,
+      default: 0
+    },
+
+    visibility: {
+      type: String,
+      default: "public"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.models.Video || mongoose.model("Video", videoSchema);
