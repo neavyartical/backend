6c3654db1fe94f666b27716639cbd500103dcef1
neavyartical <neavyartical@gmail.com>
2026-04-19 23:27:42 +0100

    call.js

--- routes/routes/routes/call.js
+++ routes/routes/routes/call.js
@@ -1,17 +1,14 @@
 const express = require("express");
+const router = express.Router();
+
 const {
   startCall,
   endCall,
   getCallHistory
 } = require("../controllers/callController");
 
-const router = express.Router();
-
-/* =========================
-   CALL ROUTES
-========================= */
 router.post("/start", startCall);
 router.post("/end", endCall);
-router.get("/history", getCallHistory);
+router.get("/:userId", getCallHistory);
 
 module.exports = router;
