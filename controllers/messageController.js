4b7075390d29541d8551409ae32ca30753760b24
neavyartical <neavyartical@gmail.com>
2026-04-23 07:02:40 +0100

    messageController.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -5,7 +5,12 @@ const Message = require("../models/Message");
 ========================= */
 const sendMessage = async (req, res) => {
   try {
-    const { sender, receiver, text } = req.body;
+    const {
+      sender,
+      receiver,
+      text,
+      messageType
+    } = req.body;
 
     if (!sender || !receiver || !text) {
       return res.status(400).json({
@@ -17,7 +22,9 @@ const sendMessage = async (req, res) => {
     const message = await Message.create({
       sender,
       receiver,
-      text
+      text,
+      messageType: messageType || "text",
+      seen: false
     });
 
     res.json({
@@ -60,7 +67,54 @@ const getMessages = async (req, res) => {
   }
 };
 
+/* =========================
+   MARK AS SEEN
+========================= */
+const markSeen = async (req, res) => {
+  try {
+    const { sender, receiver } = req.body;
+
+    await Message.updateMany(
+      {
+        sender,
+        receiver,
+        seen: false
+      },
+      {
+        seen: true
+      }
+    );
+
+    res.json({
+      success: true
+    });
+  } catch {
+    res.status(500).json({
+      success: false
+    });
+  }
+};
+
+/* =========================
+   DELETE MESSAGE
+========================= */
+const deleteMessage = async (req, res) => {
+  try {
+    await Message.findByIdAndDelete(req.params.id);
+
+    res.json({
+      success: true
+    });
+  } catch {
+    res.status(500).json({
+      success: false
+    });
+  }
+};
+
 module.exports = {
   sendMessage,
-  getMessages
+  getMessages,
+  markSeen,
+  deleteMessage
 };
